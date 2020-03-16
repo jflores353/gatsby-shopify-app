d7ab38faa3e4ce02429baacf04c153b019ff568b
Gabe <jflores353@gmail.com>
2020-03-16 13:06:17 -0500

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Shopify Shoppe`,
+    description: `Shop App built with Gatsby and Shopify!`,
+    author: `@gabefloresdev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
