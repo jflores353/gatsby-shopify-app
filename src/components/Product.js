c3c5f22adaa4cbfe927b8c251cd9d563ac5cf9b1
Gabe <jflores353@gmail.com>
2020-03-22 23:49:20 -0500

    Added Link to Product main page to redirect to Single Item purchase page.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Img from "gatsby-image"
+import { Link } from "gatsby"
 
 const Product = ({ product }) => {
   return (
-    <div>
-      <Img fixed={product.images[0].localFile.childImageSharp.fixed} />
-      <h3>{product.title}</h3>
-    </div>
+    <Link to={`/products/${product.handle}`}>
+      <div>
+        <Img fixed={product.images[0].localFile.childImageSharp.fixed} />
+        <h3>{product.title}</h3>
+      </div>
+    </Link>
   )
 }
 
